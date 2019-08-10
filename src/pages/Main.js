e524e666ee83db06487e6d22ff3195b3307fbe72
Gabriel Neto Varela Barca <gabrielvarelaneto11@gmail.com>
2019-08-10 17:33:42 -0300

    Mobile

---
+++ src/pages/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Image } from 'react-native';
+
+import logo from '../assets/logo.png';
+
+export default function Main(){
+    return (
+    <View>
+        <Image source={logo} />
+    </View>
+    );
+}
